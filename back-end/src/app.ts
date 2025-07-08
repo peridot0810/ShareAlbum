b137e6b2784a8f60f829251f92f82294d543e621
HungKungE <jonsun59@gmail.com>
2025-07-08 15:49:18 +0900

    [BE] api 요청 관련 라우팅 규칙 변경

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,7 +4,6 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index';
-import usersRouter from './routes/users';
 
 const app: Express = express();
 
@@ -13,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+// app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
 
 export default app; 
--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,10 +1,9 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import usersRouter from './users';
 
 const router = express.Router();
+router.use(express.json());
 
-/* GET home page. */
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.json({ title: 'Express API' });
-});
+router.use("/users", usersRouter);
 
-export default router; 
+export default router;
